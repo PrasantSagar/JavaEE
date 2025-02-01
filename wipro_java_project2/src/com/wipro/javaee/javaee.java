2e08ce5c55d21f445bc190b7c2200da48a0a82c1
PrasantSagar <sagarprashant2000@gmail.com>
2025-02-01 10:31:18 +0530

    commit

---
+++ wipro_java_project2/src/com/wipro/javaee/javaee.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.wipro.javaee;
+
+/**
+ * @author Sagar
+ *
+ */
+public class javaee {
+
+	/**
+	 * 
+	 */
+	public javaee() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My Second Java Project");
+
+	}
+
+}
